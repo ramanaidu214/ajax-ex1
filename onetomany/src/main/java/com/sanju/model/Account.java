445000ad1bc1efdde9fd9a4bb176bae5b11688e8
C5110610 <C5110610@HYDCTSC5110610.acsind.com>
2020-04-22 07:41:43 +0530

    one to many

--- onetomany/src/main/java/com/sanju/model/Account.java
+++ onetomany/src/main/java/com/sanju/model/Account.java
@@ -3,7 +3,6 @@ package com.sanju.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 import org.hibernate.annotations.GenericGenerator;
 
